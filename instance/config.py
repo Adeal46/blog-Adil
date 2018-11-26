5add4a0f18b1449f569354dc5a731e676f45ee7a
Adeal <adililema3@gmail.com>
2018-11-26 18:22:43 +0300

    add the credentials

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 SECRET_KEY="flasksecret"
 
-MAIL_USERNAME="donatello54611@gmail.com"
-MAIL_PASSWORD="halowars5611"
+MAIL_USERNAME="adililema3@gmail.com"
+MAIL_PASSWORD="orientally"
 
-DATABASE_URL='postgresql+psycopg2://donaldkiplagat:halowars54611@localhost/blog'
+DATABASE_URL='postgresql+psycopg2://adililema3:orientally@localhost/blog'
